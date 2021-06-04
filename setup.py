764c7d0ea348dd4828cdb4dedd6d51b3afe7f716
michaelsdr <michael.sander1295@yahoo.fr>
2021-06-04 16:03:29 +0200

    rst

--- setup.py
+++ setup.py
@@ -54,8 +54,8 @@ if __name__ == "__main__":
         version=VERSION,
         url=URL,
         download_url=DOWNLOAD_URL,
-        long_description=open("README.rst").read(),
-        long_description_content_type="text/x-rst",
+        long_description=open('README.rst').read(),
+        long_description_content_type='text/x-rst',
         classifiers=[
             "Intended Audience :: Science/Research",
             "Intended Audience :: Developers",
