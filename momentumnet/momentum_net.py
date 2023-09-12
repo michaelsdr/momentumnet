aa2c32e9bf0b9d505aa86d519dcedb95a3662855
michaelsdr <michael.sander1295@yahoo.fr>
2023-09-12 14:44:08 +0200

    add memcnn

--- momentumnet/momentum_net.py
+++ momentumnet/momentum_net.py
@@ -62,7 +62,7 @@ class MomentumNetWithBackprop(nn.Module):
                 f = f - x
             v = gamma * v + f * (1 - gamma)
             x = x + v
-        return x
+        return x, v
 
     @property
     def functions(self):
@@ -78,6 +78,7 @@ class MomentumFwd(nn.Module):
         super().__init__()
         self.function = function
         self.gamma = gamma
+        self.is_residual = is_residual
 
     def forward(self, x, v, fun_args):
         v *= self.gamma
@@ -97,114 +98,6 @@ class MomentumFwd(nn.Module):
         v += -(1 - self.gamma) * f_eval
         v /= self.gamma
         return x, v
-
-
-class MomentumTransformMemory(torch.autograd.Function):
-    @staticmethod
-    def forward(
-        ctx,
-        x,
-        v,
-        gamma,
-        functions,
-        init_function,
-        is_residual,
-        n_fun_args,
-        *params
-    ):
-        fun_args = params[:n_fun_args]
-        ctx.functions = functions
-        ctx.gamma = gamma
-        ctx.init_function = init_function
-        ctx.fun_args = fun_args
-        ctx.is_residual = is_residual
-        ctx.params_require_grad = [
-            param.requires_grad for param in params if param is not None
-        ]
-        ctx.is_residual = is_residual
-        n_iters = len(functions)
-        v = TorchExactRep(v)
-        with torch.no_grad():
-            for i in range(n_iters):
-                v *= gamma
-                f = functions[i](x, *fun_args)
-                if not is_residual:
-                    f = f - x
-                v += (1 - gamma) * f
-                x = x + v.val
-        ctx.save_for_backward(x, v.intrep, v.aux.store)
-        return x
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        functions = ctx.functions
-        gamma = ctx.gamma
-        fun_args = ctx.fun_args
-        fun_args_requires_grad = [param.requires_grad for param in fun_args]
-        n_fun_grad = sum(fun_args_requires_grad)
-        params_require_grad = ctx.params_require_grad
-        n_iters = len(functions)
-        x, v_intrep, v_store = ctx.saved_tensors
-        v = TorchExactRep(0, from_representation=(v_intrep, v_store))
-        grad_x = grad_output
-        grad_v = torch.zeros_like(grad_x)
-        grad_params = []
-        with torch.set_grad_enabled(True):
-            for i in range(n_iters):
-                function = functions[n_iters - 1 - i]
-                x = x.detach().requires_grad_(False)
-                x = x - v.val
-                x = x.detach().requires_grad_(True)
-                if ctx.is_residual:
-                    f_eval = function(x, *fun_args)
-                else:
-                    f_eval = function(x, *fun_args) - x
-                grad_combi = grad_x + grad_v
-                backward_list = []
-                for requires_grad, param in zip(
-                    params_require_grad,
-                    fun_args + tuple(find_parameters(function)),
-                ):
-                    if requires_grad:
-                        backward_list.append(param)
-                vjps = torch.autograd.grad(
-                    f_eval, (x,) + tuple(backward_list), grad_combi
-                )
-                v += -(1 - gamma) * f_eval
-                v /= gamma
-                grad_params.append([(1 - gamma) * vjp for vjp in vjps[1:]])
-                grad_x = grad_x + (1 - gamma) * vjps[0]
-                grad_v = gamma * grad_combi
-            if ctx.init_function is not None:
-                x = x.detach().requires_grad_(True)
-                f_eval = ctx.init_function(x)
-                params = tuple(find_parameters(ctx.init_function))
-                vjps = torch.autograd.grad(
-                    f_eval, params, torch.zeros_like(grad_x)
-                )
-                grad_params.append([vjp for vjp in vjps])
-            else:
-                pass
-        flat_params_vjp = []
-
-        for param in grad_params[::-1]:
-            flat_params_vjp += param[n_fun_grad:]
-        flat_param_fun = grad_params[::-1][0][:n_fun_grad]
-        for param in grad_params[::-1][1:]:
-            for j in range(n_fun_grad):
-                flat_param_fun[j] = flat_param_fun[j] + param[j]
-        flat_params_vjp = flat_param_fun + flat_params_vjp
-        flat_params = []
-        i = 0
-        for requires_grad in params_require_grad:
-            if requires_grad:
-                flat_params.append(flat_params_vjp[i])
-                i += 1
-            else:
-                flat_params.append(
-                    None
-                )  # ENH: improve this to make it cleaner
-        return (grad_x, grad_v, None, None, None, None, None, *flat_params)
 
 
 class MomentumNetNoBackprop(nn.Module):
@@ -251,33 +144,35 @@ class MomentumNetNoBackprop(nn.Module):
         self.is_residual = is_residual
         if init_function is not None:
             self.add_module("init", init_function)
+        self.nets = [
+                InvertibleModuleWrapper(
+                    MomentumFwd(functions[i], gamma, is_residual),
+                    num_bwd_passes=1,
+                    disable=False,
+                )
+                for i in range(len(functions))
+            ]
 
     def forward(self, x, *function_args):
         params = []
         functions = self.functions
         if not self.init_speed:
-            init_function = None
             v = torch.zeros_like(x)
         else:
             v = self.init_function(x)
             params += find_parameters(self.init_function)
-            init_function = self.init_function
         for function in functions:
             params += find_parameters(function)
-        n_fun_args = len(function_args)
-        output = MomentumTransformMemory.apply(
-            x,
-            v,
-            self.gamma,
-            functions,
-            init_function,
-            self.is_residual,
-            n_fun_args,
-            *function_args,
-            *params,
-        )
+        for net in self.nets: 
+            x, v = net(x.clone(), v, function_args)
         self.v = v
-        return output
+        return x, v
+
+    def inverse(self, x, v, *function_args):
+        for net in self.nets[::-1]:
+            x, v = net.inverse(x, v, function_args)
+        return x
+    
 
     @property
     def init_function(self):
@@ -286,6 +181,9 @@ class MomentumNetNoBackprop(nn.Module):
     @property
     def functions(self):
         return [self._modules[str(i)] for i in range(self.n_functions)]
+
+    def __getitem__(self, idx):
+        return self.functions[idx]
 
 
 class MomentumNet(nn.Module):
@@ -387,7 +285,8 @@ class MomentumNet(nn.Module):
         self.init_speed = init_speed
 
     def forward(self, x, *args, **kwargs):
-        return self.network.forward(x, *args)
+        x, v = self.network.forward(x, *args)
+        return x
 
     @property
     def init_function(self):
